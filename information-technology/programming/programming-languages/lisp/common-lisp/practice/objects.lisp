7ad54e9d0411e02ddfd0eba385b186daab5a0076
Mingming Li <mingmingli916@gmail.com>
2024-03-13 03:10:10 -0400

    clos

--- information-technology/programming/programming-languages/lisp/common-lisp/practice/objects.lisp
+++ information-technology/programming/programming-languages/lisp/common-lisp/practice/objects.lisp
@@ -83,3 +83,121 @@
 (defun get-next (obj name)
   (some #'(lambda (x) (gethash name x))
 	(cdr (gethash :preclist obj))))
+
+
+
+;; instance
+(defun inst (parent)
+  (let ((obj (make-hash-table)))
+    (setf (gethash :parents obj) parent)
+    obj))
+
+(defun rget (prop obj)
+  (let ((prec (gethash :preclist obj)))
+    (if prec
+	(dolist (c prec)
+	  (multiple-value-bind (val in) (gethash prop c)
+	    (if in (return (values val in)))))
+	(multiple-value-bind (val in) (gethash prop obj)
+	  (if in
+	      (values val in)
+	      (rget prop (gethash :parents obj)))))))
+
+(defun get-next (obj name)
+  (let ((prec (gethash :preclist obj)))
+    (if prec
+	(some #'(lambda (x) (gethash name x))
+	      (cdr prec))
+	(get-next (gethash obj :parents) name))))
+
+
+
+;; new implementation
+
+(defmacro parents (v) `(svref ,v 0))
+(defmacro layout (v) `(the simple-vector (svref ,v 1)))
+(defmacro preclist (v) `(svref ,v 2))
+
+(defmacro class (&optional parents &rest props)
+  `(class-fn (list ,@parents) ',props))
+
+(defun class-fn (parents props)
+  (let* ((all (union (inherit-props parents) props))
+	 (obj (make-array (+ (length all) 3)
+			  :initial-element :nil)))
+    (setf (parents obj) parents
+	  (layout obj) (coerce all 'simple-vector)
+	  (preclist obj) (precedence obj))
+    obj))
+
+(defun inherit-props (classes)
+  (delete-duplicates
+   (mapcan #'(lambda (c)
+	       (nconc (coerce (layout c) 'list)
+		     (inherit-props (parents c))))
+	   classes)))
+
+(defun precedence (obj)
+  (labels ((traverse (x)
+	     (cons x
+		   (mapcan #'traverse (parents x)))))
+    (delete-duplicates (traverse obj))))
+
+(defun inst (parent)
+  (let ((obj (copy-seq parent)))
+    (setf (parents obj) parent
+	  (preclist obj) nil)
+    (fill obj :nill :start 3)
+    obj))
+
+
+(declaim (inline lookup (setf lookup)))
+
+(defun rget (prop obj next?)
+  (let ((prec (preclist obj)))
+    ;; class
+    (if prec
+	(dolist (c (if next? (cdr prec) prec) :nil)
+	  (let ((val (lookup prop c)))
+	    (unless (eq val :nil) (return val))))
+	;; instance
+	(let ((val (lookup prop obj)))
+	  (if (eq val :nil)
+	      (rget prop (parents obj) nil)
+	      val)))))
+
+(defun lookup (prop obj)
+  (let ((off (position prop (layout obj) :test #'eq)))
+    (if off (svref obj (+ off 3)) :nil)))
+
+(defun (setf lookup) (val prop obj)
+  (let ((off (position prop (layout obj) :test #'eq)))
+    (if off
+	(setf (svref obj (+ off 3)) val)
+	(error "Can't set ~A of ~A." val obj))))
+
+
+(declaim (inline run-methods))
+
+(defmacro defprop (name &optional meth?)
+  `(progn
+     (defun ,name (obj &rest args)
+       ,(if meth?
+	    `(run-methods obj ',name args)
+	    `(rget ',name obj nil)))
+     (defun (setf ,name) (val obj)
+       (setf (lookup ',name obj) val))))
+
+(defun run-methods (obj name args)
+  (let ((meth (rget name obj nil)))
+    (if (not (eq meth :nil))
+	(apply meth obj args)
+	(error "No ~A method for ~A." name obj))))
+
+(defmacro defmeth (name obj parms &rest body)
+  (let ((gobj (gensym)))
+    `(let ((,gobj ,obj))
+       (defprop ,name t)
+       (setf (lookup ',name ,gobj)
+	     (labels ((next () (rget ,gobj ',name t)))
+	       #'(lambda ,parms ,@body))))))
